b35281557a2f77aabc3effea5623b45d49084536
mohsen <mohsenmsmdev@gmail.com>
2024-04-28 15:02:05 +0330

    context added ( dark theme )

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,20 @@ import { Route, Routes } from "react-router-dom"
 import Home from './pages/Home/Home'
 import NotFound from './pages/NotFound/NotFound'
 import Header from './components/Header/Header'
-
+import { createContext, useState } from 'react'
+export const appContext = createContext(null);
 function App() {
+
+    const [darkTheme, setDarkTheme] = useState(false);
     return (
         <>
-            <Header />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+            <appContext.Provider value={{darkTheme, setDarkTheme}}>
+                <Header />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </appContext.Provider>
         </>
     )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import style from "./header.module.css"
-function Header(){
-    return(
+import logo from "../../assets/img/logo.png"
+import avatar from "../../assets/img/avatar.png"
+import { useContext } from "react";
+import { appContext } from "../../App";
+function Header() {
+    const {dakTheme , setDarkTheme} = useContext(appContext);
+    return (
         <div className={style.container}>
-
+            <img className={style.logo} src={logo} alt="" />
+            <div className={style.menu}>
+                <div className={style.item}>
+                    <p>بازی‌ها</p>
+                </div>
+                <div className={style.item}>
+                    <p>برنامه‌ها</p>
+                </div>
+                <div className={style.item}>
+                    <p>بهترین‌ها</p>
+                </div>
+                <div className={style.item}>
+                    <p>اخبار</p>
+                </div>
+            </div>
+            <div className={style.profile}>
+                <img src={avatar} alt="" />
+            </div>
+            <div onClick={() => setDarkTheme((prev) => !prev)} className={style.themeChangeBtn}></div>
         </div>
     );
 }
